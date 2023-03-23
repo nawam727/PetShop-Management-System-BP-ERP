4881582fd481a3ad84686b3e57e978da0eaf8c25
Sadeep Amantha De Silva <sadeepamantha00@gmail.com>
2023-03-23 12:48:25 +0530

    connect database to employee UI

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace PetShop_Management_System
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Loginb());
+            Application.Run(new Customers());
         }
     }
 }
