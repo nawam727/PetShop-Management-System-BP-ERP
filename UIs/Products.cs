1bdc567bf45fa03dd546f74e80ae4c54a2059b28
NaveenSilva <naveensilva000@gmail.com>
2023-04-07 12:46:59 +0530

    adding a edit button

--- UIs/Products.cs
+++ UIs/Products.cs
@@ -21,6 +21,7 @@ namespace PetShop_Management_System.UIs
         //Connect to the database
         SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\DELL\OneDrive - NSBM\Documents\PetShopDb.mdf"";Integrated Security=True;Connect Timeout=30");
 
+        int Key = 0;
 
         private void SaveBtn_Click(object sender, EventArgs e)
         {
@@ -41,6 +42,38 @@ namespace PetShop_Management_System.UIs
                     cmd.ExecuteNonQuery();
                     MessageBox.Show("Product Added!");
                     Con.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+        }
+
+        private void EditBtn_Click(object sender, EventArgs e)
+        {
+            if (PrNameTbl.Text == "" || PrCatTbl.SelectedIndex == -1 || PrQuantityTbl.Text == "" || PrPriceTbl.Text == "")
+            {
+                MessageBox.Show("Select an Product!");
+            }
+            else
+            {
+                try
+                {
+                    Con.Open();
+                    SqlCommand cmd = new SqlCommand("update ProductTbl set PrName=@PN,PrCat=@PC,PrQty=@PQ,PrPrice=@PP where ProID=@PKey", Con);
+                    cmd.Parameters.AddWithValue("@PN", PrNameTbl.Text);
+                    cmd.Parameters.AddWithValue("@PC", PrCatTbl.SelectedItem.ToString());
+                    cmd.Parameters.AddWithValue("@PQ", PrQuantityTbl.Text);
+                    cmd.Parameters.AddWithValue("@PP", PrPriceTbl.Text);
+                    cmd.Parameters.AddWithValue("@PKey", Key);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Product Updated!");
+                    Con.Close();
+                   // DisplayProducts();
+                    //Clear();
+
                 }
                 catch (Exception ex)
                 {
